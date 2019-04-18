93ae57872500f71279cc3b852b455e7270018189
(Charlie)YanzuoGe <42125596+geyanzuo@users.noreply.github.com>
2019-04-18 16:11:20 +1000

    Add files via upload

--- ArrayExe/ArrayExe/Program.cs
+++ ArrayExe/ArrayExe/Program.cs
@@ -26,9 +26,30 @@ namespace ArrayExe
             Console.WriteLine("");
             Console.WriteLine("Task 2");
 
-            int[] num = new int[10] { 34, 5, 67, 1, 99, 34, 44, 78, 34, 0 };
+            int[] num = new int[15];
+            num[0] = 34;
+            num[1] = 5;
+            num[2] = 67;
+            num[3] = 1;
+            num[4] = 99;
+            num[5] = 34;
+            num[6] = 44;
+            num[7] = 78;
+            num[8] = 34;
+            num[9] = 0;
+
+
+            int ii = 10;
+            Console.WriteLine("We already have  34, 5, 67, 1, 99, 34, 44, 78, 34, 0 ");
+            Console.WriteLine("Type another five number ");
+            while (ii < num.Length)
+            {
+                num[ii] = int.Parse(Console.ReadLine());
+                ii++;
+            }
             
-            Console.WriteLine("Result: " + string.Join("+", num) + " = " + num.Sum());
+            
+            Console.WriteLine("Result: " + string.Join(" + ", num) + " = " + num.Sum());
             Console.ReadKey();
 
         }
