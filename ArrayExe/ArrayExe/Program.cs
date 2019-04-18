ce4b8eab5cc90206d38df96d93818b217aefcef0
(Charlie)YanzuoGe <42125596+geyanzuo@users.noreply.github.com>
2019-04-18 15:56:43 +1000

    Add files via upload

---
+++ ArrayExe/ArrayExe/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task 1");
+
+            string[] name = new string[5];
+            int i = 0;
+            Console.WriteLine("Please Type five name.....");
+            while (i < name.Length)
+            {
+                name[i] = Console.ReadLine();
+                i++;
+            }
+
+            Console.WriteLine ("Hello, " + string.Join(",", name) + "!");
+
+            Console.WriteLine("");
+            Console.WriteLine("Task 2");
+
+            int[] num = new int[10] { 34, 5, 67, 1, 99, 34, 44, 78, 34, 0 };
+            
+            Console.WriteLine("Result: " + string.Join("+", num) + " = " + num.Sum());
+            Console.ReadKey();
+
+        }
+    }
+}
